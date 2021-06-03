eb150f2c7a52db4fa28722f2075451555e83e48b
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-06-03 20:07:40 +0200

    Header Terminado Y  Menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 import "./styles.scss";
 console.log("hello world!");
+const menu = document.querySelector(".menu");
+const icono = document.querySelector(".icono--menu");
+function grande() {
+	icono.addEventListener("click", () => {
+		menu.classList.toggle("mas");
+		icono.classList.toggle("cruz");
+	});
+}
+grande();
