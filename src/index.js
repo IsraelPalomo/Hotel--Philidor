7486b0d9444a97b5e59ba9b6e4cc77858f94ac57
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-06-05 12:52:35 +0200

    Añadido formulario de reservas

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import "./styles.scss";
 console.log("hello world!");
 const menu = document.querySelector(".menu");
 const icono = document.querySelector(".icono--menu");
+const enlaceMenu = document.querySelectorAll(".enlace__menu");
 function grande() {
 	icono.addEventListener("click", () => {
 		menu.classList.toggle("mas");
 		icono.classList.toggle("cruz");
+	});
+	enlaceMenu.addEventListener("click", () => {
+		menu.classList.toggle("menos");
 	});
 }
 grande();
