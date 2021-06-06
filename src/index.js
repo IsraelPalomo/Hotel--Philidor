d75e5944fef446880d1f428034bd3348d7143673
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-06-06 12:15:24 +0200

    header terminado y seccion reservas y relax terminadas

--- src/index.js
+++ src/index.js
@@ -3,14 +3,42 @@ import "./styles.scss";
 console.log("hello world!");
 const menu = document.querySelector(".menu");
 const icono = document.querySelector(".icono--menu");
-const enlaceMenu = document.querySelectorAll(".enlace__menu");
+const enlaceMenu1 = document.querySelector(".link1");
+const enlaceMenu2 = document.querySelector(".link2");
+const enlaceMenu3 = document.querySelector(".link3");
+const enlaceMenu4 = document.querySelector(".link4");
+const enlaceMenu5 = document.querySelector(".link5");
+
 function grande() {
 	icono.addEventListener("click", () => {
 		menu.classList.toggle("mas");
 		icono.classList.toggle("cruz");
+		menu.classList.remove("menos");
 	});
-	enlaceMenu.addEventListener("click", () => {
+	enlaceMenu1.addEventListener("click", () => {
 		menu.classList.toggle("menos");
+		icono.classList.remove("cruz");
+		menu.classList.toggle("mas");
+	});
+	enlaceMenu2.addEventListener("click", () => {
+		menu.classList.toggle("menos");
+		icono.classList.remove("cruz");
+		menu.classList.toggle("mas");
+	});
+	enlaceMenu3.addEventListener("click", () => {
+		menu.classList.toggle("menos");
+		icono.classList.remove("cruz");
+		menu.classList.toggle("mas");
+	});
+	enlaceMenu4.addEventListener("click", () => {
+		menu.classList.toggle("menos");
+		icono.classList.remove("cruz");
+		menu.classList.toggle("mas");
+	});
+	enlaceMenu5.addEventListener("click", () => {
+		menu.classList.toggle("menos");
+		icono.classList.remove("cruz");
+		menu.classList.toggle("mas");
 	});
 }
 grande();
